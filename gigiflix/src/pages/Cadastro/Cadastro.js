c6f6505fb9685d49243d2604dc55bc4a711eaca9
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2020-08-01 23:26:41 -0300

    adicionando conteudo flix

--- gigiflix/src/pages/Cadastro/Cadastro.js
+++ gigiflix/src/pages/Cadastro/Cadastro.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../../components/header/header';
 
 const Cadastro = () => {
     return (
+        <>
+        <Header />
         <div>
             cadastro
         </div>
+        </>
     )
 }
 
--- gigiflix/src/pages/Home/App.js
+++ gigiflix/src/pages/Home/App.js
@@ -1,15 +1,53 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/Footer';
+import dadosInicias from '../../data/dados_iniciais.json';
+import Banner from '../../components/BannerMain';
+import Carrocel from '../../components/Carousel';
 
 const App = () => {
   return (
     <div>
-        <Header />
+      <Header />
+      <Banner
+        videoTitle={dadosInicias.categorias[4].videos[4].titulo}
+        url={dadosInicias.categorias[4].videos[4].url}
+        videoDescription={"O que é front-end? Trabalhando na área"}
+      />
 
-        <Footer />
+       <Carrocel
+        ignoreFirstVideo
+        category={dadosInicias.categorias[0]}
+      />
+
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosInicias.categorias[1]}
+      />
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosInicias.categorias[2]}
+      />
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosInicias.categorias[3]}
+      />
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosInicias.categorias[4]}
+      />
+
+      <Carrocel
+        ignoreFirstVideo
+        category={dadosInicias.categorias[5]}
+      /> 
+
+      <Footer />
     </div>
   );
 }
